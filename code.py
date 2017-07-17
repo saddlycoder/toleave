e342d767d50295fb8a1495c33119da7a19cbf81c
saddlycoder <socket.dwayne@gmail.com>
2017-07-17 17:16:31 +0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,24 @@
+# @author: vk.com/cm_3ky 
+
+import vk_api 
+
+whitelist = [138023482, 137691354, 71927597] # сообщества, которые игнорируются
+
+vk_session = vk_api.VkApi(token='TOKEN') 
+
+vk = vk_session.get_api() 
+
+gr = vk.groups.get() 
+gr = gr['items'] 
+
+
+glenght = len(gr) 
+
+i = 0 
+while i < glenght: 
+if(gr[i] in whitelist): 
+gr.append(gr[i]) 
+i+=1 
+vk.groups.leave(group_id=gr[i]) 
+i+=1 
+print 'Perfect!'
